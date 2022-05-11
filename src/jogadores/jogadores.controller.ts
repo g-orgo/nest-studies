556af983ccf93c1a67ddab76e6fb3d7ca56cfbc2
unknown <gabrielorgo5@gmail.com>
2022-05-10 22:51:47 -0300

    Utilizando o body pela primeira vez

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
-    @Post()
-    async criarAtualizarJogador (){
-        return JSON.stringify({
-            "nome": "Gabriel"
-        })
-    }
+  @Post()
+  async criarAtualizarJogador(@Body() criaJogadorDto: CriarJogadorDto) {
+    const { email } = criaJogadorDto;
+    return JSON.stringify(`email: ${email}`);
+  }
 }
