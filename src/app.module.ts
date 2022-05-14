9daa2fc0a19c6e52c71ef5ea851e09a9a091c3ee
Gabriel Orgo <gabriel.orgo@qualitor.com.br>
2022-05-14 18:37:58 -0300

    Conectando o mongodb via mongoose e ligando a interface ao schema criado para o mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://gabriel-orgo:@Mongodb323@cluster0.4q1qy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    ),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
