d09d4821423e4e6200306759b7737a75560bd09d
Gabriel Orgo <gabriel.orgo@qualitor.com.br>
2022-05-21 14:07:35 -0300

    feat: :heavy_plus_sign: Dependências de validação adicionadas e aplicadas

--- src/jogadores/dtos/criar-jogador.dto.ts
+++ src/jogadores/dtos/criar-jogador.dto.ts
@@ -1,5 +1,10 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CriarJogadorDto {
-    readonly telefoneCelular: string;
-    readonly email: string;
-    readonly nome: string;
+  @IsNotEmpty()
+  readonly telefoneCelular: string;
+  @IsEmail()
+  readonly email: string;
+  @IsNotEmpty()
+  readonly nome: string;
 }
